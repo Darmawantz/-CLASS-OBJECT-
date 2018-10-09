4e0b5e9b3d8253f1278713554bb05742a14f8e6d
Darmawantz <43660766+Darmawantz@users.noreply.github.com>
2018-10-09 22:50:11 +0700

    Create kotak.java

---
+++ kotak.java
@@ -0,0 +1,40 @@
+
+public class kotak {
+
+	 private double panjang, lebar, luas, keliling ;
+
+	public double getPanjang() {
+		return panjang;
+	}
+
+	public void setPanjang(double panjang) {
+		this.panjang = panjang;
+	}
+
+	public double getLebar() {
+		return lebar;
+	}
+
+	public void setLebar(double lebar) {
+		this.lebar = lebar;
+	}
+
+	public double getLuas() {
+		return luas;
+	}
+
+	public void setLuas(double luas) {
+		this.luas = luas;
+	}
+
+	public double getKeliling() {
+		return keliling;
+	}
+
+	public void setKeliling(double keliling) {
+		this.keliling = keliling;
+	}
+
+	
+	}
+
