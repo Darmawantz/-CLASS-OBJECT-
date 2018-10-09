2d82322868d7fb43aaf77345a1253e298c787f9c
Darmawantz <43660766+Darmawantz@users.noreply.github.com>
2018-10-09 22:51:01 +0700

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,19 @@
+
+public class Node {
+	 private String label;
+	  private int value;
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
+}
